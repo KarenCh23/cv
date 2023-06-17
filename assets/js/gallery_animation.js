a13adac9dac9a8676239e2536dd3104413f6314f
KarenCh23 <karenchamand@hotmail.fr>
2023-06-17 14:15:41 +0200

    changes on gallery animation + scrollbar custom

--- assets/js/gallery_animation.js
+++ assets/js/gallery_animation.js
@@ -1,35 +1,26 @@
-let graphicButton = document.querySelector('#graphic-btn')
-let webButton = document.querySelector('#web-btn')
+let graphicButton = document.querySelector("#graphic-btn");
+let graphicDiv = document.querySelector("#graphic-gallery");
 
 const displayTimeOut = () => {
+  setTimeout(displayAllProjects, 1000);
+};
 
- let timeout = setTimeout(DisplayAllProjects, 1000)
+const displayAllProjects = () => {
+  if (graphicDiv.style.visibility === "hidden") {
+    graphicButton.textContent = "Go back to Web projects";
+    graphicDiv.style.visibility = "visible";
+  } else {
+    graphicDiv.style.visibility = "hidden";
+    graphicButton.textContent = "See all Web & Graphic projects";
+  }
+};
 
-}
-
-const DisplayAllProjects = () => {
-
-    let graphicDiv = document.querySelector("#graphic-gallery");
-
-   if(graphicButton) {
-
-    graphicButton.innerHTML = ""
-    graphicButton.textContent = "All last projects"
-    graphicDiv.style.visibility = "visible"
-
-   }
- 
-}
-
-// Gallery photo animation //
+// Gallery photo expanded animation //
 
 let gallimages = document.querySelectorAll("#gallery-box figure");
-for(i=0; i<gallimages.length; i++) {
-  gallimages[i].addEventListener('click', function(){ 
-    this.classList.toggle("expanded"); gallimages.classList.toggle("full") 
-}); 
+for (i = 0; i < gallimages.length; i++) {
+  gallimages[i].addEventListener("click", function () {
+    this.classList.toggle("expanded");
+    gallimages.classList.toggle("full");
+  });
 }
-
-
-
-
